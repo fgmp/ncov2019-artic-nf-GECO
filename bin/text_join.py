91fd2301e7c4280d2e1e6dc52d0358de0c0abccd
Ivan-Sotelo <48335281+Ivan-Sotelo@users.noreply.github.com>
2022-04-07 19:44:29 +0800

    Update text_join.py
    links pangolin csv and nextclade together, circumvents errors when pangolin drops analysis when coverage is low

--- bin/text_join.py
+++ bin/text_join.py
@@ -3,91 +3,98 @@
 import pandas as pd
 import sys
 
-# definition of arguments
 nextclade_path = sys.argv[1]
 pangolin_path = sys.argv[2]
 redcap_meta_analysis_path = sys.argv[3]
 gisaid_authors = sys.argv[4]
 assembly_method = sys.argv[5]
 
-
-def process_csv(analyzer):
+def read_and_sort_csv(analyzer):
     #textjoins the output csv files of the analyzer and output it into a separate csv file
     if analyzer == 'pangolin':
         filename = pangolin_path
         delimiter = ','
         header = 'pangolin_string'
-        #output_file = 'pangolin_string.csv'
-        #column1 = 'taxon'
+        column1 = 'taxon'
         
     elif analyzer == 'nextclade':
         filename = nextclade_path
         delimiter = '\t'
         header = 'nextclade_string'
-        #output_file = 'nextclade_string.csv'
-        #column1 = 'seqName'
+        column1 = 'seqName'
         
     string_val = []
     line_count = 0
 
     # assign dataset
-    csvData = pd.read_csv(filename, delimiter=delimiter, header=None)
-    print(csvData)
+    csvData = pd.read_csv(filename, delimiter=delimiter)
+
+    #rename first column to central_id 
+    csvData.rename(columns={column1:'central_id'}, inplace=True)
                                          
     # sort data frame
-    csvData.sort_values([0], 
-                    axis=0,
-                    ascending=[True], 
-                    inplace=True)
+    csvData.sort_values('central_id', 
+                     axis=0,
+                     ascending=[True], 
+                     inplace=True)
+
+    #print(csvData)
+    return csvData
+
+def text_join(data_frame, header):
+    string_val = []
+    line_count = 0
+
+    for row in data_frame.iterrows():
+        row_data = data_frame.iloc[line_count].to_numpy(dtype=str)
+        string_val.append("|".join(row_data))
+        line_count += 1
     
-    #textjoin each row in the data frame
-    for row in csvData.iterrows():
-        if line_count == 0:
-            string_val.append(header)
-            line_count += 1
-            
-        else:
-            row_data = csvData.iloc[line_count-1].to_numpy(dtype=str)
-            string_val.append("|".join(row_data))
-            line_count += 1
-    
-    #write textjoined values to new csv
-    df = pd.DataFrame(string_val)
-    return df
+    text_joined_df = pd.DataFrame(string_val, columns=[header])
+
+    return text_joined_df
 
 def update_column(column,meta_output_df):  
     if column == 'gisaid_authors':
-        column_index = 5
         data = gisaid_authors
     elif column == 'assembly_method':
-        column_index = 6
         data = assembly_method
 
-    for row in range(1,len(meta_output_df)):
-        meta_output_df.loc[row, column_index] = data 
+    for row in range(0,len(meta_output_df)):
+        meta_output_df.loc[row, column] = data 
     
     return meta_output_df
-    
 
-#Merge pangolin and nextclade into one dataframe
-merged_df = pd.concat([process_csv('pangolin'), process_csv('nextclade')], axis=1)
+nextclade_df = read_and_sort_csv('nextclade')
 
-#merge meta analysis file and, nextlade and pangolin dataframe.
-#assumes meta_analysis file is always sorted by taxon
-meta_df = pd.read_csv(redcap_meta_analysis_path, delimiter=',',header=None)
+pangolin_df = read_and_sort_csv('pangolin')
+
+# merge nextclade and pangolin to link central_ids
+merged_df = pd.merge(nextclade_df, pangolin_df, 
+                on='central_id', 
+                how='outer')
+
+# split nextclade and pangolin
+nextclade_df = merged_df[list(nextclade_df.columns)]
+pangolin_df = merged_df[list(pangolin_df.columns)]
+
+# textjoin nextclade and pangolin
+nextclade_df = text_join(nextclade_df, 'nextclade_string')
+pangolin_df = text_join(pangolin_df, 'pangolin_string')
+
+# concatenate nextclade and pangolin into one dataframe
+merged_df = pd.concat([nextclade_df, pangolin_df], axis=1)
+print(merged_df)
+
+# merge meta analysis file and, nextlade and pangolin dataframe.
+# assumes meta_analysis file is always sorted by taxon
+meta_df = pd.read_csv(redcap_meta_analysis_path, delimiter=',')
 meta_output = pd.concat([meta_df, merged_df], axis=1)
 
 #update column of gisaid authors and assembly method
 meta_output = update_column('gisaid_authors',meta_output)
 meta_output = update_column('assembly_method',meta_output)
+print(meta_output)
 
 #save to csv file
-meta_output.to_csv('meta_output.csv',header=False, index=False)
-
-
-
-
-
-
-
-
+meta_output.to_csv('meta_analysis.csv', index=False)
