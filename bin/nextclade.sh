8f28431f70f741605e17cf4c5a328c5bd166b023
Ivan-Sotelo <48335281+Ivan-Sotelo@users.noreply.github.com>
2022-04-19 15:23:43 +0800

    Update nextclade.sh
    added $baseDir to make it more portable

--- bin/nextclade.sh
+++ bin/nextclade.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
+# installs latest nextclade build and says yes to prompts
+
 fasta_path=$1
-
-
-nextclade --version
-
-nextclade dataset get --name 'sars-cov-2' --output-dir '/data/apps/ncov2019-artic-nf_automated/data/sars-cov-2'
+baseDir=$2
+nextclade dataset get --name 'sars-cov-2' --output-dir "${baseDir}/data/sars-cov-2"
 
 nextclade \
    --in-order \
    --input-fasta ${fasta_path}\
-   --input-dataset /data/apps/ncov2019-artic-nf_automated/data/sars-cov-2 \
+   --input-dataset "${baseDir}/data/sars-cov-2" \
    --output-tsv nextclade.tsv
