cc5bf0417ac0b775d3e624df21c5a41f9153b544
ritmmbl <ritm.molbiolabresearch@gmail.com>
2022-05-19 15:11:27 +0800

    matched the results by merging csvs using the central_id

--- bin/text_join.py
+++ bin/text_join.py
@@ -2,6 +2,8 @@
 
 import pandas as pd
 import sys
+import numpy as np
+import re
 
 nextclade_path = sys.argv[1]
 pangolin_path = sys.argv[2]
@@ -32,13 +34,16 @@ def read_and_sort_csv(analyzer):
     #rename first column to central_id 
     csvData.rename(columns={column1:'central_id'}, inplace=True)
                                          
-    # sort data frame
-    csvData.sort_values('central_id', 
-                     axis=0,
-                     ascending=[True], 
-                     inplace=True)
+    # rename to rows to match central_ids, will get the first occurance of an integer and will delete all characters preceding it
 
-    #print(csvData)
+    for x in range(len(csvData)):
+        s1 = csvData.iloc[x]['central_id']
+        m = re.search(r"\d", s1)
+        s1 = s1[m.start()::]
+        csvData.at[x, 'central_id'] = s1
+
+
+    print(csvData)
     return csvData
 
 def text_join(data_frame, header):
@@ -69,27 +74,31 @@ nextclade_df = read_and_sort_csv('nextclade')
 
 pangolin_df = read_and_sort_csv('pangolin')
 
-# merge nextclade and pangolin to link central_ids
-merged_df = pd.merge(nextclade_df, pangolin_df, 
-                on='central_id', 
-                how='outer')
-
-# split nextclade and pangolin
-nextclade_df = merged_df[list(nextclade_df.columns)]
-pangolin_df = merged_df[list(pangolin_df.columns)]
-
-# textjoin nextclade and pangolin
-nextclade_df = text_join(nextclade_df, 'nextclade_string')
-pangolin_df = text_join(pangolin_df, 'pangolin_string')
-
-# concatenate nextclade and pangolin into one dataframe
-merged_df = pd.concat([nextclade_df, pangolin_df], axis=1)
-print(merged_df)
-
 # merge meta analysis file and, nextlade and pangolin dataframe.
 # assumes meta_analysis file is always sorted by taxon
 meta_df = pd.read_csv(redcap_meta_analysis_path, delimiter=',')
-meta_output = pd.concat([meta_df, merged_df], axis=1)
+meta_df['central_id'] = meta_df['central_id'].astype(str)
+
+# merge nextclade and meta_df to link central_ids
+merged_df = pd.merge(meta_df, pangolin_df, on='central_id', how='outer')
+
+# split pangolin_df and meta_df
+pangolin_df = merged_df[list(pangolin_df.columns)]
+
+# merge sorted pangolin_df and nextclade_df to link central ids
+merged_df = pd.merge(pangolin_df, nextclade_df, on='central_id', how='outer')
+
+# split nextclade dataframe
+nextclade_df = merged_df[list(nextclade_df.columns)]
+
+# textjoin nextclade and pangolin strings
+nextclade_df = text_join(nextclade_df, 'nextclade_string')
+pangolin_df = text_join(pangolin_df, 'pangolin_string')
+
+# concatenate nextclade and pangolin strings into one dataframe
+merged_df = pd.concat([nextclade_df, pangolin_df], axis=1)
+
+meta_output = pd.concat([meta_df, merged_df], axis=1) 
 
 #update column of gisaid authors and assembly method
 meta_output = update_column('gisaid_authors',meta_output)
