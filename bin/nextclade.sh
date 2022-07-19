07c6df52bf1a8cc76a3566df4ef01609e7c684c2
Ivan-Sotelo <48335281+Ivan-Sotelo@users.noreply.github.com>
2022-07-19 18:20:31 +0800

    Update nextclade.sh
    updated nextclade script to adapt the changes in nextclade update

--- bin/nextclade.sh
+++ bin/nextclade.sh
@@ -6,8 +6,7 @@ fasta_path=$1
 baseDir=$2
 nextclade dataset get --name 'sars-cov-2' --output-dir "${baseDir}/data/sars-cov-2"
 
-nextclade \
-   --in-order \
-   --input-fasta ${fasta_path}\
-   --input-dataset "${baseDir}/data/sars-cov-2" \
-   --output-tsv nextclade.tsv
+nextclade run \
+   -D "${baseDir}/data/sars-cov-2" \
+   --output-tsv=nextclade.tsv \
+   ${fasta_path}
